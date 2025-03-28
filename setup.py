235414d1b574115f99b5d54b9a8509d9de3f2652
subodhbarcheclover <subodh.barche@cloverinfotech.com>
2025-03-28 15:48:59 +0530

    F

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="custom_login_app",
+    version="0.0.1",
+    description="Custom Login App for Frappe",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[]
+)
